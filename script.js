c6c7f1abb57a629c21c24e56419879694a71ca2e
caio-chcn <caioh.mec@gmail.com>
2022-01-25 15:25:27 -0300

    primeira ativdade de lógica

---
+++ script.js
@@ -0,0 +1,27 @@
+// Crie uma variável que receba um nome;
+
+let nome = "caio"
+
+// Crie uma variável que receba um número;
+
+let num = 30
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let anoatual = 2022 - num
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+let resultadoSoma = num + anoatual
+console.log(resultadoSoma)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof  quartaFeira)
